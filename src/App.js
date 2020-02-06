8b7134c15355f0f63740b39ec741dfec33e574ad
Vokker <student@localhosts>
2020-02-06 15:44:31 +0300

    fixed#1

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import Tariff from './components/tarifes/Tariff'
 
 export default function App() {
     const tariffs = [
-        {title: "Мой онлайн+", price: 700, unlim:"безлимит на Tele2 России",min:"800 минут на остальные номера России",gb:"30 гигабайт интернета"},
-        {title: "Везде онлайн", price: 500, unlim:"безлимит на Tele2 России",min:"500 минут на остальные номера России",gb: "40 гигабайт интернета"},
-        {title: "Мой онлайн", price: 400, unlim:"безлимит на Tele2 России",min:"500 минут на остальные номера России",gb:"15 гигабайт интернета"}
+        { title: "Мой онлайн+", price: 700, unlim: "безлимит на Tele2 России", min: 800, mintitle: "минут на остальные номера России", gb: 30, gbtitle: "гигабайт интернета" },
+        { title: "Везде онлайн", price: 500, unlim: "безлимит на Tele2 России", min: 500, mintitle: "минут на остальные номера России", gb: 40, gbtitle: "гигабайт интернета" },
+        { title: "Мой онлайн", price: 400, unlim: "безлимит на Tele2 России", min: 500, mintitle: "минут на остальные номера России", gb: 15, gbtitle: "гигабайт интернета" }
     ]
     return (
         <div>
-            <Tariff title={tariffs[0].title} price={tariffs[0].price} unlim={tariffs[0].unlim} min={tariffs[0].min} gb={tariffs[0].gb} />
-            <Tariff title={tariffs[1].title} price={tariffs[1].price} unlim={tariffs[1].unlim} min={tariffs[1].min} gb={tariffs[1].gb} />
-            <Tariff title={tariffs[2].title} price={tariffs[2].price} unlim={tariffs[2].unlim} min={tariffs[2].min} gb={tariffs[2].gb} />
+            <Tariff title={tariffs[0].title} price={tariffs[0].price} unlim={tariffs[0].unlim} min={tariffs[0].min} mintitle={tariffs[0].mintitle} gb={tariffs[0].gb} gbtitle={tariffs[0].gbtitle} />
+            <Tariff title={tariffs[1].title} price={tariffs[1].price} unlim={tariffs[1].unlim} min={tariffs[1].min} mintitle={tariffs[1].mintitle} gb={tariffs[1].gb} gbtitle={tariffs[1].gbtitle} />
+            <Tariff title={tariffs[2].title} price={tariffs[2].price} unlim={tariffs[2].unlim} min={tariffs[2].min} mintitle={tariffs[2].mintitle} gb={tariffs[2].gb} gbtitle={tariffs[2].gbtitle} />
         </div>
     )
 }
--- src/components/tarifes/Tariff.js
+++ src/components/tarifes/Tariff.js
@@ -7,7 +7,9 @@ export default function Tariff(props) {
 <h3>{props.price}</h3>
 <h4>{props.unlim}</h4>
 <h4>{props.min}</h4>
+<h4>{props.mintitle}</h4>
 <h4>{props.gb}</h4><br/>
+<h4>{props.gbtitle}</h4>
         </div>
     )
 }
