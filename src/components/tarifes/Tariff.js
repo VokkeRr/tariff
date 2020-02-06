569e320f0e681e247eec5af59fc7177cf1fb049a
Vokker <student@localhosts>
2020-02-06 16:10:49 +0300

    fixes#2

--- src/components/tarifes/Tariff.js
+++ src/components/tarifes/Tariff.js
@@ -2,14 +2,17 @@ import React from 'react'
 
 export default function Tariff(props) {
     return (
-        <div>
-<h2>{props.title}</h2>
-<h3>{props.price}</h3>
-<h4>{props.unlim}</h4>
-<h4>{props.min}</h4>
-<h4>{props.mintitle}</h4>
-<h4>{props.gb}</h4><br/>
-<h4>{props.gbtitle}</h4>
+        <div className="main">
+            <div className="block">
+            <div className="title">{props.title}</div>
+
+<div className="price"> {props.price}</div>
+<div className="unlim"> {props.unlim}</div>
+<div className="min"> {props.min}</div>
+{props.mintitle}
+<div className="gb"> {props.gb} </div>
+{props.gbtitle}
+</div>
         </div>
     )
 }
